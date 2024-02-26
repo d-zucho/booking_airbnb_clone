a3ad79fd7a06cf5b74d97b82630b92b690c2d376
D_Zucho <danny.nico.dev@outlook.com>
2024-02-25 19:42:47 -0800

    fix axios post call -- prepare for mogo linking

--- api/index.js
+++ api/index.js
@@ -2,12 +2,19 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
+app.use(express.json())
+
 app.use(
   cors({
     origin: 'http://localhost:5173',
     credentials: true,
   })
 )
+
+app.post('/register', (req, res) => {
+  const { name, email, password } = req.body
+  res.json({ name, email, password })
+})
 
 app.get('/test', (req, res) => {
   res.json({ message: 'passZXsdfsdf!' })
